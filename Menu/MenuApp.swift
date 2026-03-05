6503f49206f30e186301a6985f88e6880eddef76
Nikola <serafimovnikola582@gmail.com>
2026-03-05 22:10:24 +0100

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Nikola Serafimov on 5.3.26.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
